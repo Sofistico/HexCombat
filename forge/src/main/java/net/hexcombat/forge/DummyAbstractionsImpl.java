1cd8b43faccfb7c3ed23d5a6644abcbecca6303e
João Victor Santos Rodrigues <joaorodrigues@bne.com.br>
2023-12-01 11:53:55 -0300

    Added the shears pattern

--- forge/src/main/java/net/hexcombat/forge/DummyAbstractionsImpl.java
+++ forge/src/main/java/net/hexcombat/forge/DummyAbstractionsImpl.java
@@ -1,8 +1,12 @@
 package net.hexcombat.forge;
 
 import net.hexcombat.DummyAbstractions;
+import net.minecraft.entity.Entity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraftforge.common.IForgeShearable;
 import net.minecraftforge.fml.loading.FMLPaths;
-
 import java.nio.file.Path;
 
 public class DummyAbstractionsImpl {
